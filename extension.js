3e9b92f04fcc909a88907479038eceddc1b7486e
Feldwor <BoQsc@users.noreply.github.com>
2018-10-14 12:04:00 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,1 +1,35 @@
+//Importing Gnome-Shell UI
+const Main = imports.ui.main;
 
+const ExtensionUtils = imports.misc.extensionUtils;
+const ME = ExtensionUtils.getCurrentExtension();
+
+const ext = ME.imports.PanelButton;
+
+
+class Extension {
+    constructor() {
+        this.label = null;
+        this.button = null;
+    }
+
+    enable() {
+        this.button = new PanelButton();
+        this.icon = new ButtonIcon();
+
+
+        this.button.set_child(this.icon);
+        // Use panel from Main namespace to insert button at the beginning of panel's right box.
+        Main.panel._rightBox.insert_child_at_index(this.button, 0);
+    }
+
+
+    disable() {
+        Main.panel._rightBox.remove_child(this.button);
+    }
+};
+
+
+function init() {
+    return new ext.Extension();
+}
