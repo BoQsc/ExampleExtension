0474af20e62bc9830a8f847ec78b479bd7ed58e9
Feldwor <BoQsc@users.noreply.github.com>
2018-10-14 11:59:35 +0300

    Update PanelButton.js

--- PanelButton.js
+++ PanelButton.js
@@ -1,1 +1,23 @@
+const St = imports.gi.St;
 
+class Button extends St.Bin {
+    constructor() {
+        super();
+        this.style_class = "panel-button";
+        this.reactive = true;
+        this.can_focus = true;
+        this.x_fill = true;
+        this.y_fill = false;
+        this.track_hover = true;
+    }
+};
+
+class ButtonIcon extends St.Icon {
+    constructor() {
+        super();
+        this.style_class = "system-run-symbolic";
+        this.icon_name = "system-run-symbolic";
+        this.icon_size = 25;
+    }
+
+};
