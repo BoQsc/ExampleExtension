a79a5db041e70145b6edabc2149b9e525f280ee5
Feldwor <BoQsc@users.noreply.github.com>
2018-10-17 19:23:57 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,35 +1,30 @@
+"use strict"
 //Importing Native Modules 
 const ExtensionUtils = imports.misc.extensionUtils;
 const ME = ExtensionUtils.getCurrentExtension();
-const panel = ME.imports.panel;
+const panel = ME.imports.PanelButton;
 const Main = imports.ui.main;
 
 
 class Extension {
-constructor() {}
+    constructor() {}
 
 
- enable() {
+    enable() {
 
         this.button = new panel.Button();
-        this.icon = new panel.ButtonIcon();
-        // Add icon inside the button
-this.button.set_child(this.icon);
-  panel._showHello();
-this.button.connect('button-press-event', panel._showHello);
-Main.panel._rightBox.insert_child_at_index(this.button, 0);
- }
+        let GnomeTopPanel = Main.panel;
+        GnomeTopPanel._rightBox.insert_child_at_index(this.button, 0)
+        panel._showHello();
+    }
 
 
- disable() {
- Main.panel._rightBox.remove_child(this.button);
- }
+    disable() {
+        Main.panel._rightBox.remove_child(this.button);
+    }
 
 }
 
 function init() {
     return new Extension();
 }
-
-
-
